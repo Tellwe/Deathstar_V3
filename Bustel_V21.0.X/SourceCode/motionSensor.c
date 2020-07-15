e111f9036f6c929c89fffbaf492112f3551a7b31
Sebastian <sebastian.tellwe@gmail.com>
2020-07-15 19:30:03 +0200

    Bugfix. make sure that the motion input goes high and then low before starting the cycle

--- Bustel_V21.0.X/SourceCode/motionSensor.c
+++ Bustel_V21.0.X/SourceCode/motionSensor.c
@@ -6,6 +6,7 @@ static int debounceCounter = 0;
 static int numberOfSuccessfullDetections = 50;
 static int debouncePeriodMilliSeconds = 10;
 static unsigned int *localMillisecondCounterPtr;
+static int motionSensorInputHighDetected = 0;
 
 //Motion Sensor input
 #define motionSensorSignal			RA1
@@ -19,7 +20,7 @@ int motionSensorUpdate()
 {
 	static unsigned int begin;
 	//If the active-state of the motion sensor has been detected, initiate debounce sequence
-	if(motionSensorSignal == 1)
+	if(motionSensorSignal == 1 && motionSensorInputHighDetected == 0)
 	{
 		unsigned int var = *localMillisecondCounterPtr - begin;
 		if(debounceCounter == 0) 
@@ -29,7 +30,7 @@ int motionSensorUpdate()
 		}
 		else if(debounceCounter >= numberOfSuccessfullDetections)
 		{
-			motionSensorState = 1;
+			motionSensorInputHighDetected = 1;
 		}
 		else if(var >= debouncePeriodMilliSeconds)
 		{
@@ -37,6 +38,11 @@ int motionSensorUpdate()
 			debounceCounter++;
 
 		}
+	}
+	else if(motionSensorSignal == 0 && motionSensorInputHighDetected == 1)
+	{
+		motionSensorInputHighDetected = 0;
+		motionSensorState = 1;
 	}
 	else
 	{
